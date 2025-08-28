cf3cd576190dbfaf1c19f61acd695437a881d3a6
AleGxrcia <fagarciatavarez@gmail.com>
2025-08-28 17:28:09 -0400

    feat: configure project references and dependencies

--- backend/src/API/CinemaBookingSystem.API/obj/Debug/net8.0/CinemaBookingSystem.API.AssemblyInfo.cs
+++ backend/src/API/CinemaBookingSystem.API/obj/Debug/net8.0/CinemaBookingSystem.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaBookingSystem.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+631ea0f293a9317172c23ef6d9d768d1ee40d7b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+558bd51b8e7abf3adbff115312deb1d6707d65b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaBookingSystem.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaBookingSystem.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
