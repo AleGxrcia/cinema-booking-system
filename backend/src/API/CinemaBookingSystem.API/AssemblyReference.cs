766e3a2ab51a38fd28f6779c55a84542a63b7a05
AleGxrcia <fagarciatavarez@gmail.com>
2025-09-23 19:47:30 -0400

    feat: create API module extensions and assembly reference

---
+++ backend/src/API/CinemaBookingSystem.API/AssemblyReference.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+namespace CinemaBookingSystem.Api;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Program = typeof(Program).Assembly;
+}
